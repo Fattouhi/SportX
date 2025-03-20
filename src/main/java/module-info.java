f52e5bb7417e1d407349ea46f0baf61a745cc0a3
Fattouhi <jasserfattouhi8@gmail.com>
2025-03-20 11:20:29 +0100

    Resolved merge conflict in module-info.java wefgfdwevdew

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,25 +11,21 @@ module com.example.sportmarket {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
-    opens com.example.sportmarket to javafx.fxml;
-    opens com.example.sportx to javafx.graphics;
-    opens com.example.sportx.controller.abir to javafx.fxml;
-
-    exports com.example.chatboot to javafx.fxml;
-    opens com.example.chatboot to javafx.fxml;
-
-    exports com.example.sportmarket.DAO;
-    opens com.example.sportmarket.DAO to javafx.fxml;
-    exports com.example.sportmarket;
-
     requires org.json;
-    requires mysql.connector.java;
+    //requires mysql.connector.java;
     requires org.jsoup;
     requires org.slf4j;
     requires java.net.http;
     requires com.fasterxml.jackson.databind;
     requires java.sql;
     requires java.desktop;
+
+
+    opens com.example.sportmarket to javafx.fxml;
+    exports com.example.sportmarket.DAO;
+    opens com.example.sportmarket.DAO to javafx.fxml;
+    exports com.example.sportmarket;
+
 
     opens com.example.sportxnews to javafx.fxml;
     exports com.example.sportxnews;
@@ -39,5 +35,15 @@ module com.example.sportmarket {
     opens com.example.sportxnews.dao to javafx.fxml;
     exports com.example.sportxnews.models;
     opens com.example.sportxnews.models to javafx.fxml;
+
+
+    // Open the package containing NavbarController to javafx.fxml
+    opens com.example.sportx.controller.abir to javafx.fxml;
+
+    // Export the package containing the CommunityController
+    exports com.example.sportx.controller.abir to javafx.fxml;
+
+
+
 
 }
